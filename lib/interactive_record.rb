24a0d84f0d8049985ea7770539c9e177b63fd94b
Jonathan Hong <jonhong777@gmail.com>
2018-08-07 16:59:21 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 class InteractiveRecord
-
+  def self.table_name
+    self.class.downcase.pluralize
+  end
 end
